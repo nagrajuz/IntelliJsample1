2925f864c934ca0e234b5ede6d758e89f003aef3
nagrajuz <nagbdps@gmail.com>
2022-04-24 17:18:25 +0530

    initial commit 4

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hELLO");
-
+        System.out.println("World");
+        System.out.println("Universe");
+        System.out.println("Solar");
     }
 }
